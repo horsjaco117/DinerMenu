95ae95636e0fe2b60e453babf8f05e71bbab85af
horsjaco117 <jacob_horsley@live.com>
2025-03-19 15:25:51 -0600

    meal descriptions added

--- DinerMenu/Form1.vb
+++ DinerMenu/Form1.vb
@@ -7,13 +7,19 @@ Public Class Form1
     End Sub
 
     Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
-        FoodLabel.Text = "The Liquid Lunch"
+        FoodLabel.Text = "The Liquid Lunch" & vbCrLf & "The liquid lunch is a true man's way of putting" _
+            & vbCrLf & "barnacles on your chest. Specially cured from our oil vats" _
+            & vbCrLf & "off the coast of shen zen China. Try it only if you dare..."
     End Sub
     Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
-        FoodLabel.Text = "Sus Salad"
+        FoodLabel.Text = "Sus Salad" & vbCrLf & "This salad seperates the men from the Weeny hut juniors'" &
+            "Salads include the tentacles from the jellyfish from Jellyfish Fields."
+
     End Sub
     Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
-        FoodLabel.Text = "Chumbalaya"
+        FoodLabel.Text = "Chumbalaya" & vbCrLf & "With the aquirement of the Chum Bucket this classic meal" _
+            & "is now available here at the Salty Spitoon."
+
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
